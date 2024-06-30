d099cadf720cc42b1cafc85c4037b30aa5b6398a
DMan GG <daniel.gg@zoho.com>
2024-06-30 17:33:03 +0200

    feat(add): WhatIsSepsis

--- src/components/Home/WhatIsSepsis.js
+++ src/components/Home/WhatIsSepsis.js
@@ -3,16 +3,15 @@ import { Box, Typography, Container } from '@mui/material';
 
 const WhatIsSepsis = () => {
     return (
-        <section class='whatissepsis'>
+        <section className='whatissepsis'>
             <Box className="flexible-content-full-width-content page-content">
                 <Container className="page-content-wrap" sx={{ background: '#fff', boxShadow: '0px 0px 29.44px 2.56px rgba(1, 1, 1, 0.5)'}}>
-                    <Box className="section-title" sx={{ mb: 2 }}>
-                        <Typography variant="h3">What is Sepsis?</Typography>
+                    <Box className="section-title">
+                        <Typography variant="h4">¿Qué es la Sepsis?</Typography>
                     </Box>
-                    <Box className="section-content" sx={{ p: 3, border: '2px solid #fff' }}>
-                        <Typography variant="body1">
-                            Sepsis, which was often called “blood poisoning,” is a life-threatening emergency that happens when your body’s response to an infection damages vital organs and, often, causes death. Learn more about the symptoms of sepsis, which kills 350,000 adults each year in the United States.
-                        </Typography>
+                    <Box className="section-content" sx={{border: '2px solid #fff' }}>
+                        <Typography className='p' variant="body1">
+                        La sepsis, anteriormente conocida como "envenenamiento de la sangre," es una emergencia médica que pone en peligro la vida y ocurre cuando la respuesta del cuerpo a una infección daña órganos vitales y, a menudo, causa la muerte. Aprende más sobre los síntomas de la sepsis, que mata a más de 17,000 personas cada año en España.                        </Typography>
                     </Box>
                 </Container>
             </Box>
